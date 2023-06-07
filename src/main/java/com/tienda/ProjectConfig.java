d0a41700126705231f82447d7a963a45ccb85335
Laboratorios <Laboratorios@172.16.6.8>
2023-06-06 21:04:28 -0600

    Semana04

---
+++ src/main/java/com/tienda/ProjectConfig.java
@@ -0,0 +1,42 @@
+package com.tienda;
+
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+
+
+
+
+
+@Configuration
+public class ProjectConfig implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localResolver(){
+       var slr = new SessionLocaleResolver();
+       
+       slr.setDefaultLocale(Locale.getDefault());
+       slr.setLocaleAttributeName("session.current.locale");
+       slr.setTimeZoneAttributeName("session.current.timezone");
+       
+       return slr;
+    }
+    
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        var lci = new LocaleChangeInterceptor();
+        
+        lci.setParamName("lang");
+        return lci;
+    }
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registro){
+        registro.addInterceptor(localeChangeInterceptor());
+    }
+}
