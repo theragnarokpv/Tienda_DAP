2285f2fe3730df2034d8d8302147331816da9873
Laboratorios <Laboratorios@172.16.6.7>
2023-06-13 20:45:14 -0600

    Sem05

---
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ *
+ * @author Laboratorios
+ */
+
+@Data
+@Entity
+@Table(name="categoria")
+
+public class Categoria implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    @Column(name="id_categoria")
+    private Long idCategoria;
+    private String descripcion;
+    private String rutaImagen;
+    private boolean activo;
+    
+}
