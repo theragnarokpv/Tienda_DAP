c26970b48a082d0f63a6cb0fc8dde2a6fe483fad
Laboratorios <Laboratorios@172.16.6.13>
2023-07-25 20:29:34 -0600

    Semana11

--- src/main/java/com/tienda/dao/UsuarioDao.java
+++ src/main/java/com/tienda/dao/UsuarioDao.java
@@ -13,4 +13,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface UsuarioDao extends JpaRepository <Usuario, Long>{
     Usuario findByUsername(String username);
+    
+    Usuario findByUsernameAndPassword(String username, String Password);
+
+    Usuario findByUsernameOrCorreo(String username, String correo);
+
+    boolean existsByUsernameOrCorreo(String username, String correo);
+    
 }
---
+++ src/main/java/com/tienda/service/CorreoService.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.tienda.service;
+
+/**
+ *
+ * @author Laboratorios
+ */
+import jakarta.mail.MessagingException;
+
+public interface CorreoService {
+    public void enviarCorreoHtml(
+            String para, 
+            String asunto, 
+            String contenidoHtml) 
+            throws MessagingException;
+}
